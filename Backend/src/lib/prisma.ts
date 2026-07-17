45f1e2537a5d48c2f05f4dfe9d64aefb4832f8ee
yash-khandelwal-codr <yash.khandelwal.codr@gmail.com>
2026-07-17 16:45:41 +0530

    add Prisma adapter and client setup for PostgreSQL

---
+++ Backend/src/lib/prisma.ts
@@ -0,0 +1,14 @@
+// src/lib/prisma.ts
+
+import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+const prisma = new PrismaClient({
+  adapter,
+});
+
+export default prisma;
